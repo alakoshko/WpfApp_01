e1b2ba0a4e1258130ded61d8fb5c2a9cfe0fd9a1
alakoshko <alakoshko@gmail.com>
2018-10-18 03:22:32 +0300

    добавлен контролер Департаментов и Компаний

---
+++ WebApiServer/Controllers/CompaniesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataModel;
+using System.Collections.ObjectModel;
+
+namespace WebApiServer.Controllers
+{
+    public class CompaniesController : ApiController
+    {
+        public SQLProcessing sqlProcessing;
+
+        private static ObservableCollection<Company> _Companies = new ObservableCollection<Company>();
+
+        private CompaniesController()
+        {
+            sqlProcessing = new SQLProcessing();
+            ////Company
+            sqlProcessing.GetCompanies(_Companies);
+        }
+
+        public IEnumerable<Company> GetAllDepartaments() => _Companies;
+
+        public IHttpActionResult GetEmployer(Guid guid)
+        {
+            var employe = _Companies.First(p => p.ID == guid);
+            if (employe is null) return NotFound();
+            return Ok(employe);
+        }
+    }
+}
+
---
+++ WebApiServer/Controllers/DeptsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataModel;
+using System.Collections.ObjectModel;
+
+namespace WebApiServer.Controllers
+{
+    public class DeptsController : ApiController
+    {
+        public SQLProcessing sqlProcessing;
+
+        private static ObservableCollection<Department> _Depts = new ObservableCollection<Department>();
+
+        private DeptsController()
+        {
+            sqlProcessing = new SQLProcessing();
+
+            ////Departaments
+            sqlProcessing.GetDepartments(_Depts);
+        }
+
+        public IEnumerable<Department> GetAllDepartaments() => _Depts;
+
+        public IHttpActionResult GetEmployer(Guid guid)
+        {
+            var employe = _Depts.First(p => p.ID == guid);
+            if (employe is null) return NotFound();
+            return Ok(employe);
+        }
+    }
+}
